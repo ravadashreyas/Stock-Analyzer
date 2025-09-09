5a3582ddc539e49e8e8f15b32faff05ef98d5be3
ravadashreyas <ravada.s@northeastern.edu>
2025-09-09 21:57:33 +0100

    Update trade.py

--- trade.py
+++ trade.py
@@ -4,13 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as pl
 
 x = "AAPL"
-data = yf.download(x, start="2023-01-01", end="2023-12-31", interval="1d")
+data = yf.download(x, start="2025-09-01", end="2025-9-8", interval="1d")
 print(data.head())
 
 close_prices_df = data[["Close"]].reset_index()
 print(close_prices_df.head())
 
 
-data["Close"].plot(title="Apple Closing Prices")
-plt.show()
+#data["Close"].plot(title="Apple Closing Prices")
+#plt.show()
 
