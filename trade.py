c3500e9eecf5246e2467e53e4d59876ce4150a18
ravadashreyas <ravada.s@northeastern.edu>
2025-09-10 17:35:29 +0100

    Update trade.py

--- trade.py
+++ trade.py
@@ -1,26 +1,59 @@
+from pandas._libs.tslibs import Tick
 import yfinance as yf
 import pandasql as ps
 import pandas as pd
 import matplotlib.pyplot as plt
 
 x = "AAPL"
-data = yf.download(x, start="2022-01-01", end="2025-9-8", interval="1d")
-print(data.head())
+def stockInfo(x):
+#downloading YF data
 
-data["SMA_20"] = data["Close"].rolling(window=20).mean()
-data["SMA_50"] = data["Close"].rolling(window=50).mean()
-data["SMA_200"] = data["Close"].rolling(window=200).mean()
-close_prices_df = data[["Close"]].reset_index()
-sorted_df = data.sort_values(by="Date", ascending=False)
-#sorted_df = close_prices_df.drop(columns=["Date"])
+    stock = yf.download(x, start="2022-01-01", end="2025-9-8", interval="1d")
+    if isinstance(stock.columns, pd.MultiIndex):
+        stock.columns = stock.columns.get_level_values(0)
+   
 
+    #getting moving avg
+    stock["SMA_20"] = stock["Close"].rolling(window=20).mean()
+    stock["SMA_50"] = stock["Close"].rolling(window=50).mean()
+    stock["SMA_200"] = stock["Close"].rolling(window=200).mean()
 
-data["Close"].plot(title="Apple Closing Prices")
-data["SMA_20"].plot(title="Apple Closing Prices")
-data["SMA_50"].plot(title="Apple Closing Prices")
-data["SMA_200"].plot(title="Apple Closing Prices")
-plt.legend()
-plt.show()
+    #get closing prices only
+    close_prices_df = stock[["Close"]].reset_index()
+    #sort by date (others dont work)
+    sorted_df = stock.sort_values(by="Date", ascending=False)
+    #sorted_df = close_prices_df.drop(columns=["Date"])
+    return stock
 
+stock = stockInfo(x)
 
+def sortData(stock):
+    sorted_df = stock.sort_values(by="Close", ascending=False)
+    print(sorted_df.head()) 
+
+def checkCond(stock):
+    #check condition for column
+    close = stock[["Close"]].reset_index()
+    close["Above 230"] = close["Close"] > 230
+    true = close[close["Above 230"] == True]
+    #print(true.head(50))
+
+    #finding all time high
+    max_close = close["Close"].max()
+    print(max_close)
+
+def plotData(stock):
+    #How to Plot the data
+    stock["Close"].plot(title="Apple Closing Prices")
+    stock["SMA_20"].plot()
+    stock["SMA_50"].plot()
+    stock["SMA_200"].plot()
+    plt.xlabel("Date")
+    plt.ylabel("Price")
+    plt.legend()
+    plt.show()
+
+stockInfo(x)
+checkCond(stock)
+plotData(stock)
 
