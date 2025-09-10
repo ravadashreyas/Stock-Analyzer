6bb42b6c0efbf14530c16b8a735cbb0ecfe86c30
ravadashreyas <ravada.s@northeastern.edu>
2025-09-10 14:16:46 +0100

    Update trade.py

--- trade.py
+++ trade.py
@@ -1,16 +1,26 @@
 import yfinance as yf
 import pandasql as ps
 import pandas as pd
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 
 x = "AAPL"
-data = yf.download(x, start="2025-09-01", end="2025-9-8", interval="1d")
+data = yf.download(x, start="2022-01-01", end="2025-9-8", interval="1d")
 print(data.head())
 
+data["SMA_20"] = data["Close"].rolling(window=20).mean()
+data["SMA_50"] = data["Close"].rolling(window=50).mean()
+data["SMA_200"] = data["Close"].rolling(window=200).mean()
 close_prices_df = data[["Close"]].reset_index()
-print(close_prices_df.head())
+sorted_df = data.sort_values(by="Date", ascending=False)
+#sorted_df = close_prices_df.drop(columns=["Date"])
 
 
-#data["Close"].plot(title="Apple Closing Prices")
-#plt.show()
+data["Close"].plot(title="Apple Closing Prices")
+data["SMA_20"].plot(title="Apple Closing Prices")
+data["SMA_50"].plot(title="Apple Closing Prices")
+data["SMA_200"].plot(title="Apple Closing Prices")
+plt.legend()
+plt.show()
+
+
 
